e6f164dd9dc7e586ed5b410cf7c7a49e7cc42170
Sevrinn <sevrinnsevrinn@gmail.com>
2021-07-24 20:48:37 -0700

    added handleAddTodo function and corresponding onSubmit on form element

--- src/App.js
+++ src/App.js
@@ -8,11 +8,39 @@ function App() {
   //(we store our todoObjects here eventually)
   const[todoList, setTodoList] = useState([]);
 
+  // - - - - - - C R E A T E - - - - - - //
+  // handleAddTodo is called when add todo button is clicked and it:
+  //1. prevents the page from reloading
+  //2. prevents empty todos from being added
+  //3. creates a newTodoObj
+  //4. sets todolist to include the newTodoObj and then the rest of the todoList
+  //5. resets Todo to an empty string
+
+  const handleAddTodo =(e) => {
+    //1.
+    e.preventDefault();
+    //2.
+    if(todo.length === 0){
+      return;
+    }
+    //3.
+    const newTodoObj = {
+      todo: todo,
+      complete: false,
+    }
+    //4.
+    setTodoList([newTodoObj, ...todoList])
+    //5.
+    setTodo("")
+  }
+
   return (
     <div className="App">
       <h1>Single Component Todo List</h1>
       <p>Add a todo:</p>
-      <form>
+      {/* When fomr is submited, the anonymous callback
+      takes the event and passes it to the handleAddtodo function above */}
+      <form onSubmit={(e)=>handleAddTodo(e) }>
         {/* when input changes, it takes in the event and updates todo
           to equal whats in the input box */}
         <input 
